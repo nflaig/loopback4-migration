74e392d76997f0c717195c35fa779e8641ec8af5
nflaig <nflaig@protonmail.com>
2020-07-05 18:47:09 +0200

    Refactor repository integration test

--- src/__tests__/integration/repositories/migration.repository.integration.ts
+++ src/__tests__/integration/repositories/migration.repository.integration.ts
@@ -7,7 +7,8 @@ import {
     migrationRepository,
     getApplication,
     migrateSchema,
-    omit
+    omit,
+    TestApplication
 } from "../../helpers";
 import { MockMigrationScript } from "../../fixtures/migrations";
 import { testdb } from "../../fixtures/datasources";
@@ -15,9 +16,12 @@ import { MigrationAction } from "../../../types";
 import { MigrationRepository } from "../../../repositories";
 
 describe("MigrationRepository (integration)", () => {
+    let application: TestApplication;
+
     before(migrateSchema);
 
     beforeEach(givenEmptyDatabase);
+    beforeEach(givenApplication);
 
     after(givenEmptyDatabase);
 
@@ -25,7 +29,7 @@ describe("MigrationRepository (integration)", () => {
         it("should use the existing data source of the application if no data source name is specified", () => {
             const existingdb = new juggler.DataSource({ name: "existingdb" });
 
-            const repository = new MigrationRepository(getApplication(), [existingdb]);
+            const repository = new MigrationRepository(application, [existingdb]);
 
             expect(repository.dataSource).to.equal(existingdb);
         });
@@ -33,7 +37,7 @@ describe("MigrationRepository (integration)", () => {
         it("should use the data source specified in the configuration", () => {
             const otherdb = new juggler.DataSource({ name: "otherdb" });
 
-            const repository = new MigrationRepository(getApplication(), [otherdb, testdb], {
+            const repository = new MigrationRepository(application, [otherdb, testdb], {
                 dataSourceName: testdb.name
             });
 
@@ -44,7 +48,7 @@ describe("MigrationRepository (integration)", () => {
         it("should use the model name specified in the configuration", () => {
             const modelName = "Test";
             const existingdb = new juggler.DataSource({ name: "existingdb" });
-            const repository = new MigrationRepository(getApplication(), [existingdb], {
+            const repository = new MigrationRepository(application, [existingdb], {
                 modelName
             });
 
@@ -55,7 +59,7 @@ describe("MigrationRepository (integration)", () => {
         it("should throw an error if the data source with the given name does not exist", () => {
             const nonExistingdbName = "nonexistingdb";
             expect(() => {
-                new MigrationRepository(getApplication(), [], {
+                new MigrationRepository(application, [], {
                     dataSourceName: nonExistingdbName
                 });
             }).to.throwError(`Did not find data source with name ${nonExistingdbName}`);
@@ -63,7 +67,7 @@ describe("MigrationRepository (integration)", () => {
 
         it("should throw an error if there are no data sources", () => {
             expect(() => {
-                new MigrationRepository(getApplication());
+                new MigrationRepository(application);
             }).to.throwError("Did not find any data source");
         });
     });
@@ -97,4 +101,8 @@ describe("MigrationRepository (integration)", () => {
             validateMigration(createdMigration, { ...migrationScript, action, changeNumber: 1 });
         });
     });
+
+    function givenApplication() {
+        application = getApplication();
+    }
 });
