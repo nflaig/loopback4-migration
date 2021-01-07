c17e315921217c510cf2b7b94d938baa4477ffdf
nflaig <nflaig@protonmail.com>
2021-01-07 18:30:09 +0100

    Add RepositoryComponent interface to component class

--- src/component.ts
+++ src/component.ts
@@ -1,4 +1,5 @@
 import { Component, bind, ContextTags, createServiceBinding } from "@loopback/core";
+import { RepositoryComponent } from "@loopback/repository";
 import { Migration } from "./models";
 import { MigrationObserver } from "./observers";
 import { MigrationRepository } from "./repositories";
@@ -7,7 +8,7 @@ import { MigrationBindings } from "./keys";
 import { MigrationBooter } from "./booters";
 
 @bind({ tags: { [ContextTags.KEY]: MigrationBindings.COMPONENT.key } })
-export class MigrationComponent implements Component {
+export class MigrationComponent implements Component, RepositoryComponent {
     models = [Migration];
     repositories = [MigrationRepository];
     lifeCycleObservers = [MigrationObserver];
